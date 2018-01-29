bedcf74c815078a39e15416326a28d8be54a30f6
Weiqing Chen <kevincwq@gmail.com>
2018-01-30 01:23:48 +0800

    ...

--- Code/Balancer/Balancer.ino
+++ Code/Balancer/Balancer.ino
@@ -5,21 +5,23 @@
 
 //#define DEBUG
 
-const int battery_low_threshold1 = 1050, battery_high_threshold1 = 800, diode_voltage_compensation = 83;
+const int battery_low_threshold1 = 1110, battery_high_threshold1 = 800, diode_voltage_compensation = 83;
 const unsigned long loop_time = 4000, voltage_loop_time = 5000000;
-const int calibration_loops = 500;
 const int acc_raw_limit = 8200;
 // const int motor_interval = 500;
-const int motor_speed = 200;   // rpms
+const int motor_speed = 120;   // rpms
 const int gyro_address = 0x68; //MPU-6050 I2C address (0x68 or 0x69)
 const int dead_band = 5;
 
 //Various settings
-float pid_p_gain = 11;        //Gain setting for the P-controller (15)
-float pid_i_gain = 0.5;         //Gain setting for the I-controller (1.5)
-float pid_d_gain = 25;        //Gain setting for the D-controller (30)
-float turning_speed = 40;     //Turning speed (20)
-float max_target_speed = 120; //Max target speed (100)
+// 1: 9, 0.5, 3
+// 2: 19, 0.6, 3
+// 3:
+float pid_p_gain = 20;       //Gain setting for the P-controller (15)
+float pid_i_gain = 0.6;      //Gain setting for the I-controller (1.5)
+float pid_d_gain = 3;        //Gain setting for the D-controller (30)
+float turning_speed = 40;    //Turning speed (20)
+float max_target_speed = 40; //Max target speed (100)
 
 unsigned long time, next_loop_time, last_voltage_loop_time; //, left_motor_start_time, right_motor_start_time;
 unsigned long gyro_timer;
@@ -40,11 +42,48 @@ Stepper stepperR(200, 8, 9, 10, 12);
 int battery_voltage;
 int receive_counter;
 int gyro_pitch_data_raw, gyro_yaw_data_raw, accelerometer_data_raw;
-long gyro_yaw_calibration_value, gyro_pitch_calibration_value, acc_calibration_value = -200;
+long gyro_yaw_calibration_value = -521, gyro_pitch_calibration_value = -21, acc_calibration_value = -400;
 
 float angle_gyro, angle_acc, angle, self_balance_pid_setpoint;
 float pid_error_temp, pid_i_mem, pid_setpoint, gyro_input, pid_output, pid_last_d_error;
 float pid_output_left, pid_output_right;
+
+void calibrate()
+{
+  gyro_yaw_calibration_value = 0;
+  gyro_pitch_calibration_value = 0;
+  acc_calibration_value = 0;
+  int calibration_loops = 200;
+  for (int counter = 0; counter < calibration_loops; counter++)
+  {
+    //Create calibration_loops loops
+    if (counter % 10 == 0)
+      digitalWrite(13, !digitalRead(13));                           //Change the state of the LED every 15 loops to make the LED blink fast
+    Wire.beginTransmission(gyro_address);                           //Start communication with the gyro
+    Wire.write(0x43);                                               //Start reading the Who_am_I register 75h
+    Wire.endTransmission();                                         //End the transmission
+    Wire.requestFrom(gyro_address, 4);                              //Request 2 bytes from the gyro
+    gyro_yaw_calibration_value += Wire.read() << 8 | Wire.read();   //Combine the two bytes to make one integer
+    gyro_pitch_calibration_value += Wire.read() << 8 | Wire.read(); //Combine the two bytes to make one integer
+
+    Wire.beginTransmission(gyro_address);
+    Wire.write(0x3F);
+    Wire.endTransmission();
+    Wire.requestFrom(gyro_address, 2);
+    acc_calibration_value += Wire.read() << 8 | Wire.read();
+    delay(10);
+  }
+  gyro_pitch_calibration_value /= calibration_loops; //Divide the total value by 500 to get the avarage gyro offset
+  gyro_yaw_calibration_value /= calibration_loops;   //Divide the total value by 500 to get the avarage gyro offset
+  acc_calibration_value /= calibration_loops;
+
+  Serial.print(F("gyro_pitch_calib:"));
+  Serial.println(gyro_pitch_calibration_value);
+  Serial.print(F("gyro_yaw_calib:"));
+  Serial.println(gyro_yaw_calibration_value);
+  Serial.print(F("acc_calib:"));
+  Serial.println(acc_calibration_value);
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Setup basic functions
@@ -78,40 +117,9 @@ void setup()
 
   pinMode(13, OUTPUT); //Configure digital poort 13 as output
 
-  for (receive_counter = 0; receive_counter < calibration_loops; receive_counter++)
-  {
-    //Create calibration_loops loops
-    if (receive_counter % 15 == 0)
-      digitalWrite(13, !digitalRead(13));                           //Change the state of the LED every 15 loops to make the LED blink fast
-    Wire.beginTransmission(gyro_address);                           //Start communication with the gyro
-    Wire.write(0x43);                                               //Start reading the Who_am_I register 75h
-    Wire.endTransmission();                                         //End the transmission
-    Wire.requestFrom(gyro_address, 4);                              //Request 2 bytes from the gyro
-    gyro_yaw_calibration_value += Wire.read() << 8 | Wire.read();   //Combine the two bytes to make one integer
-    gyro_pitch_calibration_value += Wire.read() << 8 | Wire.read(); //Combine the two bytes to make one integer
-
-    // Wire.beginTransmission(gyro_address);
-    // Wire.write(0x3F);
-    // Wire.endTransmission();
-    // Wire.requestFrom(gyro_address, 2);
-    // acc_calibration_value += Wire.read() << 8 | Wire.read();
-    delayMicroseconds(loop_time - 300); //Wait for 3700 microseconds to simulate the main program loop time
-  }
-  gyro_pitch_calibration_value /= calibration_loops; //Divide the total value by 500 to get the avarage gyro offset
-  gyro_yaw_calibration_value /= calibration_loops;   //Divide the total value by 500 to get the avarage gyro offset
-  // acc_calibration_value /= calibration_loops;
-
   stepperL.setSpeed(motor_speed);
   stepperR.setSpeed(motor_speed);
 
-#ifdef DEBUG
-  Serial.print(F("gyro_pitch_calib:"));
-  Serial.println(gyro_pitch_calibration_value);
-  Serial.print(F("gyro_yaw_calib:"));
-  Serial.println(gyro_yaw_calibration_value);
-  Serial.print(F("acc_calib:"));
-  Serial.println(acc_calibration_value);
-#endif
   gyro_timer = micros();
   next_loop_time = micros() + loop_time;
 }
@@ -147,10 +155,8 @@ void loop()
     {                         //If batteryvoltage is below 10.5V and higher than 8.0V
       digitalWrite(13, HIGH); //Turn on the led if battery voltage is too low
       low_bat = 1;            //Set the low_bat variable to 1
-#ifdef DEBUG
       Serial.print(F("low battery voltage:"));
       Serial.println(battery_voltage);
-#endif
     }
     last_voltage_loop_time = t;
   }
@@ -174,7 +180,7 @@ void loop()
   Serial.println(angle_acc);
 #endif
 
-  if (start == 0 && angle_acc > -0.5 && angle_acc < 0.5)
+  if (start == 0 && angle_acc > -0.3 && angle_acc < 0.3)
   {
     //If the accelerometer angle is almost 0
     angle_gyro = angle_acc; //Load the accelerometer angle in the angle_gyro variable
@@ -211,7 +217,7 @@ void loop()
   //Uncomment the following line to make the compensation active
   //angle_gyro -= gyro_yaw_data_raw * 0.0000003;                            //Compensate the gyro offset when the robot is rotating
 
-  angle_gyro = angle_gyro * 0.99 + angle_acc * 0.01; //Correct the drift of the gyro angle with the accelerometer angle
+  angle_gyro = angle_gyro * 0.9997 + angle_acc * 0.0003; //Correct the drift of the gyro angle with the accelerometer angle
 #ifdef DEBUG
   Serial.print(F("comp ang:"));
   Serial.println(angle_gyro);
@@ -225,7 +231,7 @@ void loop()
   //The (pid_setpoint - pid_output * 0.015) part functions as a brake function.
   pid_error_temp = angle_gyro - self_balance_pid_setpoint - pid_setpoint;
   if (pid_output > 10 || pid_output < -10)
-    pid_error_temp += pid_output * 0.015;
+    pid_error_temp += pid_output * 0.005;
 
   pid_i_mem += pid_i_gain * pid_error_temp;    //Calculate the I-controller value and add it to the pid_i_mem variable
   pid_i_mem = constrain(pid_i_mem, -400, 400); //Limit the I-controller to the maximum controller output
@@ -253,65 +259,72 @@ void loop()
   pid_output_left = pid_output;  //Copy the controller output to the pid_output_left variable for the left motor
   pid_output_right = pid_output; //Copy the controller output to the pid_output_right variable for the right motor
 
-  if (received_byte & B00000001)
-  {                                    //If the first bit of the receive byte is set change the left and right variable to turn the robot to the left
-    pid_output_left += turning_speed;  //Increase the left motor speed
-    pid_output_right -= turning_speed; //Decrease the right motor speed
-  }
-  if (received_byte & B00000010)
-  {                                    //If the second bit of the receive byte is set change the left and right variable to turn the robot to the right
-    pid_output_left -= turning_speed;  //Decrease the left motor speed
-    pid_output_right += turning_speed; //Increase the right motor speed
-  }
+  if (start == 1)
+  {
+    if (received_byte & B00000001)
+    {                                    //If the first bit of the receive byte is set change the left and right variable to turn the robot to the left
+      pid_output_left += turning_speed;  //Increase the left motor speed
+      pid_output_right -= turning_speed; //Decrease the right motor speed
+    }
+    if (received_byte & B00000010)
+    {                                    //If the second bit of the receive byte is set change the left and right variable to turn the robot to the right
+      pid_output_left -= turning_speed;  //Decrease the left motor speed
+      pid_output_right += turning_speed; //Increase the right motor speed
+    }
 
-  if (received_byte & B00000100)
-  { //If the third bit of the receive byte is set change the left and right variable to turn the robot to the right
-    if (pid_setpoint > -2)
-      pid_setpoint -= 0.05; //Slowly change the setpoint angle so the robot starts leaning forewards
-    if (pid_output > max_target_speed * -1)
-      pid_setpoint -= 0.005; //Slowly change the setpoint angle so the robot starts leaning forewards
-  }
-  if (received_byte & B00001000)
-  { //If the forth bit of the receive byte is set change the left and right variable to turn the robot to the right
-    if (pid_setpoint < 2)
-      pid_setpoint += 0.05; //Slowly change the setpoint angle so the robot starts leaning backwards
-    if (pid_output < max_target_speed)
-      pid_setpoint += 0.005; //Slowly change the setpoint angle so the robot starts leaning backwards
-  }
+    if (received_byte & B00000100)
+    { //If the third bit of the receive byte is set change the left and right variable to turn the robot to the right
+      if (pid_setpoint > -1)
+        pid_setpoint -= 0.02; //Slowly change the setpoint angle so the robot starts leaning forewards
+      if (pid_output > max_target_speed * -1)
+        pid_setpoint -= 0.005; //Slowly change the setpoint angle so the robot starts leaning forewards
+      if (pid_setpoint < -2)
+        pid_setpoint += 0.1;
+    }
+    if (received_byte & B00001000)
+    { //If the forth bit of the receive byte is set change the left and right variable to turn the robot to the right
+      if (pid_setpoint < 1)
+        pid_setpoint += 0.02; //Slowly change the setpoint angle so the robot starts leaning backwards
+      if (pid_output < max_target_speed)
+        pid_setpoint += 0.005; //Slowly change the setpoint angle so the robot starts leaning backwards
+      if (pid_setpoint > 2)
+        pid_setpoint -= 0.1;
+    }
 
-  if (!(received_byte & B00001100))
-  { //Slowly reduce the setpoint to zero if no foreward or backward command is given
-    if (pid_setpoint > 0.5)
-      pid_setpoint -= 0.05; //If the PID setpoint is larger then 0.5 reduce the setpoint with 0.05 every loop
-    else if (pid_setpoint < -0.5)
-      pid_setpoint += 0.05; //If the PID setpoint is smaller then -0.5 increase the setpoint with 0.05 every loop
-    else
-      pid_setpoint = 0; //If the PID setpoint is smaller then 0.5 or larger then -0.5 set the setpoint to 0
+    if (!(received_byte & B00001100))
+    { //Slowly reduce the setpoint to zero if no foreward or backward command is given
+      if (pid_setpoint > 0.5)
+        pid_setpoint -= 0.05; //If the PID setpoint is larger then 0.5 reduce the setpoint with 0.05 every loop
+      else if (pid_setpoint < -0.5)
+        pid_setpoint += 0.05; //If the PID setpoint is smaller then -0.5 increase the setpoint with 0.05 every loop
+      else
+        pid_setpoint = 0; //If the PID setpoint is smaller then 0.5 or larger then -0.5 set the setpoint to 0
+    }
+
+    //The self balancing point is adjusted when there is not forward or backwards movement from the transmitter.
+    //This way the robot will always find it's balancing point
+    if (pid_setpoint == 0)
+    { //If the setpoint is zero degrees
+      if (pid_output < 0)
+        self_balance_pid_setpoint += 0.005; //Increase the self_balance_pid_setpoint if the robot is still moving forewards
+      if (pid_output > 0)
+        self_balance_pid_setpoint -= 0.005; //Decrease the self_balance_pid_setpoint if the robot is still moving backwards
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Motor pulse calculations
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //To compensate for the non-linear behaviour of the stepper motors the folowing calculations are needed to get a linear speed behaviour.
+    if (pid_output_left > 0)
+      pid_output_left = 405 - (1 / (pid_output_left + 9)) * 5500;
+    else if (pid_output_left < 0)
+      pid_output_left = -405 - (1 / (pid_output_left - 9)) * 5500;
+
+    if (pid_output_right > 0)
+      pid_output_right = 405 - (1 / (pid_output_right + 9)) * 5500;
+    else if (pid_output_right < 0)
+      pid_output_right = -405 - (1 / (pid_output_right - 9)) * 5500;
   }
-
-  //The self balancing point is adjusted when there is not forward or backwards movement from the transmitter.
-  //This way the robot will always find it's balancing point
-  if (pid_setpoint == 0)
-  { //If the setpoint is zero degrees
-    if (pid_output < 0)
-      self_balance_pid_setpoint += 0.0015; //Increase the self_balance_pid_setpoint if the robot is still moving forewards
-    if (pid_output > 0)
-      self_balance_pid_setpoint -= 0.0015; //Decrease the self_balance_pid_setpoint if the robot is still moving backwards
-  }
-
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //Motor pulse calculations
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //To compensate for the non-linear behaviour of the stepper motors the folowing calculations are needed to get a linear speed behaviour.
-  if (pid_output_left > 0)
-    pid_output_left = 405 - (1 / (pid_output_left + 9)) * 5500;
-  else if (pid_output_left < 0)
-    pid_output_left = -405 - (1 / (pid_output_left - 9)) * 5500;
-
-  if (pid_output_right > 0)
-    pid_output_right = 405 - (1 / (pid_output_right + 9)) * 5500;
-  else if (pid_output_right < 0)
-    pid_output_right = -405 - (1 / (pid_output_right - 9)) * 5500;
 
   //Calculate the needed pulse time for the left and right stepper motor controllers
   if (pid_output_left > 0)
@@ -388,7 +401,7 @@ void run()
       left_step = 0;
     }
   }
-  else if (throttle_counter_left_motor == 1)
+  else if (throttle_counter_left_motor == 1 && left_step != 0)
   {
     stepperL.step(left_step);
   }
@@ -413,7 +426,7 @@ void run()
       right_step = 0;
     }
   }
-  else if (throttle_counter_right_motor == 1)
+  else if (throttle_counter_right_motor == 1 && right_step != 0)
   {
     stepperR.step(right_step);
   }
